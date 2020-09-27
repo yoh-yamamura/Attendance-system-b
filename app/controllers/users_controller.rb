24ad51b71ec0862680b6c5ffa4585fe3f8819149
EC2 Default User <ec2-user@ip-172-31-26-2.us-east-2.compute.internal>
2020-09-27 10:45:07 +0000

    5.5.7ユーザー登録、保存

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = "新規作成に成功しました。"
+      redirect_to @user
       # 保存に成功した場合は、ここに記述した処理が実行されます。
     else
       render :new
