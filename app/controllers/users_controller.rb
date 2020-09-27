c6275382e0cd60c1b5ee1f8e4593307d589d2453
EC2 Default User <ec2-user@ip-172-31-26-2.us-east-2.compute.internal>
2020-09-27 10:19:08 +0000

    5.5.3Strong-Parameters

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,19 @@ class UsersController < ApplicationController
   def new
     @user = User.new # ユーザーオブジェクトを生成し、インスタンス変数に代入します
   end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # 保存に成功した場合は、ここに記述した処理が実行されます。
+    else
+      render :new
+    end
+  end  
+  
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
