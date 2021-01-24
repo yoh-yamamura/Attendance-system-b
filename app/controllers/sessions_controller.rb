f516a98876aaf47bfb2b78c0569ad598778603b9
EC2 Default User <ec2-user@ip-172-31-26-2.us-east-2.compute.internal>
2021-01-24 03:40:09 +0000

    6.4一時的にログイン状態。

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,8 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       # ログイン後にユーザー情報ページにリダイレクトします。
+      log_in user
+      redirect_to user
     else
       # ここにはエラーメッセージ用のflashを入れます。
       flash.now[:danger] = '認証に失敗しました。'
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,19 @@
 module SessionsHelper
-end
+  
+  # 引数に渡されたユーザーオブジェクトでログインします。
+  def log_in(user)
+    session[:user_id]
+  end 
+  
+  # 現在ログイン中のユーザーがいる場合オブジェクトを返します。
+  def current_user
+    if session[:user_id]
+      @current_user ||= User.find_by(id: session[:user_id])
+    end
+  end
+  
+  # 現在ログイン中のユーザーがいればtrue、そうでなければfalseを返します。
+  def logged_in?
+    !current_user.nil?
+  end
+end  
