bfb5e0c70b2a30d5f5c8d2d4ebb770d92d13a90d
EC2 Default User <ec2-user@ip-172-31-26-2.us-east-2.compute.internal>
2021-01-24 03:50:32 +0000

    ログイン機能をアクションに追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user # 保存成功後、ログインします。
       flash[:success] = "新規作成に成功しました。"
       redirect_to @user
       # 保存に成功した場合は、ここに記述した処理が実行されます。
