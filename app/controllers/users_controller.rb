227c14cbcfb815d2c3507cb9daf64e471f202b7b
yoh-yamamura <yyoh864@gmail.com>
2021-02-06 04:45:04 +0000

    Finish japanization-and-time-format

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
   before_action :logged_in_user, only: [:index, :show, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update]
   before_action :admin_user, only: :destroy
@@ -9,7 +9,6 @@ class UsersController < ApplicationController
    end
   
   def show
-    @user = User.find(params[:id])
   end
   
   def new
@@ -33,13 +32,13 @@ class UsersController < ApplicationController
   end
   
   def update
-    @user = User.find(params[:id])
-    if @user.update_attributes
+    if @user.update_attributes(user_params)
       flash[:success] = "ユーザー情報を更新しました。"
       redirect_to @user
     else
       render :edit
     end  
+  end  
     
     def destroy
       @user.destroy
@@ -47,8 +46,6 @@ class UsersController < ApplicationController
       redirect_to users_url
     end
     
-  end  
-  
   private
 
     def user_params
@@ -65,6 +62,7 @@ class UsersController < ApplicationController
     # ログイン済みのユーザーか確認します。
     def logged_in_user
       unless logged_in?
+        store_location
         flash[:danger] = "ログインしてください。"
         redirect_to login_url
       end
