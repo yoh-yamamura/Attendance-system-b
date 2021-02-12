72bf860ba55f5ea64d93f648e40c9a7de37fd036
yoh-yamamura <yyoh864@gmail.com>
2021-02-12 12:51:39 +0000

    9.3.1モーダルウィンドウを表示しよう（ルーティング追加）

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,18 @@
 Rails.application.routes.draw do
-
   root 'static_pages#top'
   get '/signup', to: 'users#new'
-  
-   # ログイン機能
+
+  # ログイン機能
   get    '/login', to: 'sessions#new'
   post   '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-  
-  resources :users
+
+  resources :users do
+    get '/edit_basic_all', to: 'users#edit_basic_all'  
+    post '/update_basic_all', to: 'users#update_basic_all'
+    member do
+      get 'edit_basic_info'
+      patch 'update_basic_info'
+    end
+  end
 end
